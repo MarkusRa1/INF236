c94490be8258978e2ed6b7ac89e885317627d9a6
Markus Ragnhildstveit <markus.ragn@gmail.com>
2019-01-29 12:34:48 +0100

    parallel init

---
+++ ProblemSet1/Parallel/Cellular1D-Parallel.c
@@ -0,0 +1,22 @@
+#include < mpi .h > // Header defining MPI functions
+#include < stdio .h >
+#include < string .h >
+int main(int argc, char **argv)
+{
+    int comm_sz;            /* Number of processes */
+    int my_rank;            /* My process rank */
+    MPI_Init(&argc, &argv); // No MPI function before this call
+    MPI_Comm_size(MPI_COMM_WORLD, &comm_sz);
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+    if (my_rank != 0)
+    {
+        printf("rank %d", my_rank);
+    }
+    else
+    {
+        printf("rank 0");
+    }
+    // Code
+    MPI_Finalize(); // No MPI function after this call
+    return 0;
+}
