c0b789506e778bab5232182e2cbe2073cfe61b33
Markus Ragnhildstveit <markus.ragn@gmail.com>
2019-02-15 14:55:43 +0100

    added better speed perf

--- ProblemSet1/Parallel/Cellular2D-Parallel.c
+++ ProblemSet1/Parallel/Cellular2D-Parallel.c
@@ -112,7 +112,8 @@ int main(int argc, char **argv)
     {
         double t1 = (end1 - start);
         double t2 = (end2 - start);
-        printf("size: %d\niterations: %d\ntime taken: %fs, %fs\n", sz, numOfIt, t1, t2);
+        printf("%lu bits/bytes\n", sizeof(short));
+        printf("size: %d\niterations: %d\ntime taken: %fs, %fs\nSpeed: %fMBps\n", sz, numOfIt, t1, t2, ((double) sz*sz*numOfIt)/t2*sizeof(short)/1000000);
         // printMatrix(historyall, w, h);
         // writeToFile("de", historyall, w, h);
         free(historyall);
